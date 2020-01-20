a1ed0c878aa8f73a15e044356121945fd524ce1c
hubert <hubert.b.laganiere@gmail.com>
2020-01-19 23:47:19 -0500

    Inputhandler not finish for getDirection

--- include/inputHandler.h
+++ include/inputHandler.h
@@ -10,16 +10,17 @@
 
 const int UP_ARROW[] = {27, 91, 65} ;
 const int DOWN_ARROW[] = {27, 91, 66} ;
-const int LEFT_ARROW[] = {27, 91, 65} ;
-const int RIGHT_ARROW[] = {27, 91, 65} ;
+const int LEFT_ARROW[] = {27, 91, 68} ;
+const int RIGHT_ARROW[] = {27, 91, 67} ;
 
 class InputHandler /*: public boost::signals2::signal_base*/{
     public:
         InputHandler(){};
         ~InputHandler(){};
-        Direction getDirection();
+        Direction getDirection() const;
     private:
-        int getKeyCode();
+        int getKeyCode() const;
+        bool isCodeIn(int code, const int neededCode[], int index) const;
 };
 
 
--- src/inputHandler.cpp
+++ src/inputHandler.cpp
@@ -1,12 +1,30 @@
 #include "inputHandler.h" 
 
 #include <iostream>
-Direction InputHandler::getDirection(){
-    getKeyCode();
+Direction InputHandler::getDirection() const{
+    int code = getKeyCode();
+    int direction = 0b1111;
+    int i =0;
+    while( i < 3){
+        
+        // il faut faire un mask pour avoir la bonne fonction logique
+        direction & this->isCodeIn(code, UP_ARROW, i) << Direction::Up;
+        direction & this->isCodeIn(code, DOWN_ARROW, i) << Direction::Down;
+        direction & this->isCodeIn(code, LEFT_ARROW, i) << Direction::Left;
+        direction & this->isCodeIn(code, RIGHT_ARROW, i) << Direction::Right;
+        
+        if (direction == 0){ // none has been pressed, lets start over
+            direction = 0b1111;
+            i = 0;
+        } else { // keep going
+            ++i;
+        }
+    }
+    // doit s'occuper du return aussi
     return Direction::Up;
 }
 
-int* InputHandler::getKeyCode(){
+int InputHandler::getKeyCode() const{
     // https://stackoverflow.com/questions/3276546/how-to-implement-getch-function-of-c-in-linux
     struct termios oldattr, newattr;
     int ch;
@@ -14,15 +32,11 @@ int* InputHandler::getKeyCode(){
     newattr = oldattr;
     newattr.c_lflag &= ~( ICANON | ECHO );
     tcsetattr( STDIN_FILENO, TCSANOW, &newattr );
-    
-    ch = getchar();
-    std::cout << ch <<std::endl;
     ch = getchar();    
-    std::cout << ch <<std::endl;
-
-    ch = getchar();    
-    std::cout << ch <<std::endl;
-    
     tcsetattr( STDIN_FILENO, TCSANOW, &oldattr );
     return ch;
+}
+
+bool InputHandler::isCodeIn(int code, const int  neededCode[], int index) const{
+    return code == neededCode[index];
 }
